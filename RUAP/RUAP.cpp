02c2e3b4f9f8508ff3c76fb10887eac90e5d3f35
Nikola <nstipic@etfos.hr>
2021-10-26 18:17:38 +0200

    Izmjena poruke

--- RUAP/RUAP.cpp
+++ RUAP/RUAP.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Andreje Gojic\n";
+    std::cout << "Moj broj indeksa\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
